eba664d6149c86f4ac45df47a9642ab6a89f3de7
sand2104 <prasadsandy2000@gmail.com>
2023-07-11 11:58:17 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoToExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName;
+            Console.WriteLine("Enter User Name");
+            userName = Console.ReadLine();
+            if(userName.Length>=6)
+            {
+                Console.WriteLine("Welcome : \t" + userName);
+            }
+            else
+            {
+                Console.WriteLine("Invalid User Name");
+            }
+            Console.ReadKey();
+        }
+    }
+}
